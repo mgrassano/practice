276b3e89dc388c80910e6849b4ff296532f20b6e
Mary Grassano <mrg977@yahoo.com>
2021-11-29 20:51:49 -0700

    feat: added prime number functions and tests

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
 "use strict";
 
-// prime number is greater than 1 w only 2 factors - themselves and 1
-// a prime number cannot be divided by any other numbers without leaving a remainder
-// ex. 13 has only itself and 1
-// ex. 4 has itself, 2, and 1 NOT a prime number
-// list of prime numbers to 100
-// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-
 function isPrime(n) {
-  if (n === 1) {
+  if (n === 1 || n === 0) {
     return false;
   } else if (n === 2) {
     return true;
   } else {
-    for (let i = 0; i < n; i++) {
+    for (let i = 2; i < n; i++) {
       if (n % i === 0) {
         return false;
       }
     }
+    return true;
   }
-  return true;
 }
 
 exports.isPrime = isPrime;
 
 function getPrimeNumbers(n) {
-  const isPrime = [];
-  for (let i = 0; i < n; i++) {
-    isPrime.push(i);
+  const primeNumbers = [];
+  for (let i = 0; i <= n; i++) {
+    if (isPrime(i)) {
+      primeNumbers.push(i);
+    }
   }
+  return primeNumbers;
 }
-return isPrime;
+
+exports.getPrimeNumbers = getPrimeNumbers;
--- test/main.js
+++ test/main.js
@@ -4,12 +4,24 @@ var main = require("../main");
 describe("main", function () {
   describe("isPrime", function () {
     it("should return true for a prime number", function () {
-      const value = main.isPrime(97);
+      const value = main.isPrime(5);
       assert.equal(value, true);
     });
     it("should return false for a non-prime number", function () {
-      const value = main.isPrime(99);
+      const value = main.isPrime(0);
       assert.equal(value, false);
+    });
+  });
+  describe("getPrimeNumbers", function () {
+    it("should return a list of prime numbers up to n", function () {
+      const value = main.getPrimeNumbers(100);
+      assert.deepStrictEqual(
+        value,
+        [
+          2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+          67, 71, 73, 79, 83, 89, 97,
+        ]
+      );
     });
   });
 });
